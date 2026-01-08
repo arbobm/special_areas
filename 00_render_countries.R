f8347ee94dea7ca52db7c9c2ba694eae1657b1b5
arbobm <brunameneses@gmail.com>
2026-01-08 10:25:06 +0000

    Initial commit

---
+++ 00_render_countries.R
@@ -0,0 +1,63 @@
+
+
+# packages ----------------------------------------------------------------
+
+# load packages
+
+library(readr)
+library(stringr)
+
+
+# countries list ----------------------------------------------------------
+
+countries_list <- c(
+  'Argentina',
+  'Philippines',
+  # 'Indonesia',
+  'Cambodia',
+  'Bangladesh',
+  'Kyrgyzstan',
+  'Kazakhstan',
+  'Turkmenistan',
+  'Ecuador'
+) |> 
+  sort()
+
+
+# read template -----------------------------------------------------------
+
+input  <- "template.qmd"            # your source .qmd
+
+# create chapters for each country ----------------------------------------
+## >>>> can I make this loop work in parallel? <<<<<<<<<
+for (i in 1:length(countries_list)) {
+  
+  country <- countries_list[i]
+  message(country)
+  
+  # Paths
+  output <- paste0(str_to_lower(country), ".qmd")   # new file to write
+  
+  # Read as text (lines)
+  lines <- read_file(input)
+  
+  # Replace: "windfarm" -> "wind farm" everywhere (case-sensitive)
+  # Use gsub for global replacement
+  lines_new <- gsub("'country_i'", country, lines)
+  lines_new <- gsub("country_i", paste0("'", country, "'"), lines_new)
+  
+  # Write
+  write_file(lines_new, output)
+
+}
+
+
+# final tasks -------------------------------------------------------------
+
+# check if all countries are in _quarto.yml chapters
+
+# render doc with chapters
+
+# quarto::quarto_preview(browse = F, watch = F)
+
+# quarto::quarto_render(output_format = 'html')
